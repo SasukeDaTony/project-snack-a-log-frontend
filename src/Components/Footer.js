bfa0df027220d63a46dfe600ab39045718e38822
SasukeDaTony <tonytaylor@pursuit.org>
2023-02-05 14:59:05 -0500

    final

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,16 +1,24 @@
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
+export default function Footer() {
+  return (
+    <div className="m-top light-salmon footer">
+      <p className="flex">
+        <div className="red">
+          <FavoriteIcon />{" "}
+        </div>{" "}
+        Represents a healthy snack where if the fiber or protein or both is
+        5 grams or higher and the sugar is less than 6 grams
+      </p>
 
-
-
-export default function Footer(){
-    return (
-        <div className='m-top light-salmon footer'>
-            <p className='flex'><div className='red'><FavoriteIcon/>  </div>   Represents a healthy snack where fiber is greater than 5g, protein is greater than 5g, and sugar is less than 5g</p>
-
-            <p className='flex neg-mtop'><div className='white'><FavoriteBorderIcon/>  </div>   Represents an unhealthy snack</p>
-
-        </div>
-    )
+      <p className="flex neg-mtop">
+        <div className="white">
+          <FavoriteBorderIcon />{" "}
+        </div>{" "}
+        Represents an unhealthy snack where the sugar is 6 grams or more and if the
+        sugar, protein, & fiber is less than 5 grams
+      </p>
+    </div>
+  );
 }
