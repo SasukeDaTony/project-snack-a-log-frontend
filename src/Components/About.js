e480a92bf261562fac0375e7b1a5ce4811a7eee1
SasukeDaTony <tonytaylor@pursuit.org>
2023-02-05 12:57:29 -0500

    about page final

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,14 +1,41 @@
 export default function About() {
   return (
-    <div>
+    <div className="about">
       <h1 className="title">Snack-A-Log Collaborators Group 5</h1>
       <div className="zay">
         <h3>Zay Lors</h3>
-        <p>Zay Bio</p>
+        <p>
+          I am an aspiring software engineer from Brooklyn, New York. My
+          passions include drumming, playing and watching chess, boxing, and NBA
+          basketball. I am a twin and the second child of four. I can speak 4
+          languages. English is my primary language, however, being of Haitian
+          descent, I am fluent in Haitian-Creole. I've been programming
+          Javascript, React, SQL, for a year and excited to embark on my journey
+          as a software developer! I can be found at <br />
+          <a href="https://github.com/ZayLpursuit">Github</a>, <br />
+          EMAIL: ZayLors@pursuit.org. <br />
+        </p>
       </div>
       <div className="jean">
         <h3>Jean Napoleon</h3>
-        <p>Jean Bio</p>
+        <p>
+          I am a Full-Stack developer living in Queens. From a young age I've
+          had a keen interest in coding and technology in general. One of my
+          early childhood dreams was to be a YouTuber reviewing high-end
+          consumer technology. My technical skills include Javascript, REACT,
+          SQL. This website was a collaborative effort with some friends,
+          competent programmers in their own right, but I have done numerous
+          solo projects that showcases my technical acumen. Please refer to my
+          Github for examples.If you have any questions or professional
+          interest, I can be found at <br />
+          <a href="https://github.com/BigDaddyJean">Github</a>,{" "}
+          <a href="https://www.linkedin.com/in/jean-napoleon-a95734247/">
+            LinkedIn
+          </a>{" "}
+          <br />
+          EMAIL: Jeannappoleon@pursuit.org. <br />
+          Thank you and have a great day.
+        </p>
       </div>
       <div className="tony">
         <h3>Anthony Taylor</h3>
@@ -19,7 +46,12 @@ export default function About() {
           Electronics in vehicles instead of applying technological concepts &
           programming languages with computers, which is what I really desired.I
           found the Pursuit Fellowship Organization and began my path to
-          becoming a Javascript developer.
+          becoming a Javascript developer with currently working with the PERN
+          Stack for web development. I can be found at <br />
+          <a href="https://github.com/SasukeDaTony">Github</a>,{" "}
+          <a href="https://www.linkedin.com/in/tonytaylor91/">LinkedIn</a>{" "}
+          <br />
+          EMAIL: TonyTaylor@pursuit.org. <br />
         </p>
       </div>
     </div>
--- src/Components/SnackCard.js
+++ src/Components/SnackCard.js
@@ -1,17 +1,23 @@
-import { useNavigate } from "react-router-dom"
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { useNavigate } from "react-router-dom";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
-export default function SnackCard({snack}){
-    const navigate=useNavigate()
-    return (
-        <div className="card" onClick={()=>navigate(`/snacks/${snack.id}`)}>
-            <img src={snack.image} alt="" className="img-size"/>
-            <h3>{snack.name}</h3>
+export default function SnackCard({ snack }) {
+  const navigate = useNavigate();
+  return (
+    <div className="card" onClick={() => navigate(`/snacks/${snack.id}`)}>
+      <img src={snack.image} alt="snack image" className="img-size" />
+      <h3>{snack.name}</h3>
 
-            {snack.is_healthy===true?( <div className="red"><FavoriteIcon/>  </div>):(<div className="white"><FavoriteBorderIcon/>  </div>) }
-            
+      {snack.is_healthy === true ? (
+        <div className="red">
+          <FavoriteIcon />{" "}
         </div>
-    )
-
+      ) : (
+        <div className="white">
+          <FavoriteBorderIcon />{" "}
+        </div>
+      )}
+    </div>
+  );
 }
